a5a2966298e122319e923bd83ab8300f4380ee15
islam <islamachrouf2004@gmail.com>
2024-12-02 10:15:56 +0100

    Debut d'analyseur lexical

---
+++ syntax.tab.h
@@ -0,0 +1,49 @@
+#ifndef SYNTAX_TAB_H
+#define SYNTAX_TAB_H
+
+
+#define DEBUT 
+#define EXECUTION 
+#define FIN 
+#define NUM 
+#define REAL 
+#define SI 
+#define ALORS 
+#define TEXT 
+#define ELSE 
+#define TANTQUE 
+#define FAIRE 
+#define ET_LOGIQUE 
+#define OU_LOGIQUE 
+#define NEGATION 
+#define FIXE 
+#define AFFICHE 
+#define LIRE 
+#define EGAL 
+#define DIFFERENT 
+#define ASSIGNATION 
+#define SUPERIEUR 
+#define SUP_EGAL 
+#define INFERIEUR 
+#define INF_EGAL
+#define AFFECTATION 
+#define PLUS 
+#define MOINS
+#define FOIS 
+#define DIVISER 
+#define POINT_VIRGULE 
+#define DEUX_POINTS 
+#define VIRGULE 
+#define PARENTHESE_OUVRANTE 
+#define PARENTHESE_FERMANTE 
+#define CROCHET_OUVRANT 
+#define CROCHET_FERMANT 
+#define ACCOLADE_OUVRANTE 
+#define ACCOLADE_FERMANTE 
+#define ID 
+#define CST
+#define DOUBLE_SHARP
+#define STRING
+ 
+ 
+#endif
